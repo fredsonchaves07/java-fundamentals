7300187fee49ec2979a601a4e13fdc377a98808a
fredsonchaves07 <fredsonchaves07@gmail.com>
2021-12-17 17:10:58 -0300

    feat: add chalenges

---
+++ src/chalenges/WorkedHours.java
@@ -0,0 +1,22 @@
+package chalenges;
+
+import java.util.Scanner;
+
+public class WorkedHours {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int numberHours = scanner.nextInt();
+        double valueHours = scanner.nextDouble();
+
+        double salary = numberHours * valueHours;
+
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = U$%.2f%n", salary);
+
+        scanner.close();
+    }
+}
