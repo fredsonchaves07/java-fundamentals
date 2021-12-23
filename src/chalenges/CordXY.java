9b43be0278aff312e304ba63b25b8b479f7b1a72
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-23 15:34:05 -0300

    challenges with repetition structure while

---
+++ src/chalenges/CordXY.java
@@ -0,0 +1,31 @@
+package chalenges;
+
+import java.util.Scanner;
+
+public class CordXY {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        while(x != 0 && y != 0){
+            if(x > 0 && y > 0){
+                System.out.println("Primeiro");
+            } else if (x > 0 && y < 0){
+                System.out.println("Quarto");
+            } else if (x < 0 && y > 0){
+                System.out.println("Segundo");
+            } else {
+                System.out.println("Terceiro");
+            }
+
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+        }
+
+        scanner.close();
+    }
+}
