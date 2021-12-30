563d7dd29afba2d10e958cd0e36b71a503cad69b
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-29 22:03:20 -0300

    feat: add area calcularion method

--- src/poo/Triangle.java
+++ src/poo/Triangle.java
@@ -7,9 +7,11 @@ public class Triangle {
     public double a;
     public double b;
     public double c;
-    public double p = (a + b + c) / 2.0;
-    public double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
+    public double calcArea(){
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,10 +29,10 @@ public class Triangle {
         y.b = scanner.nextDouble();
         y.c = scanner.nextDouble();
 
-        System.out.println("Area of triangle x: " + x.area);
-        System.out.println("Area of triangle y: " + y.area);
+        System.out.println("Area of triangle x: " + x.calcArea());
+        System.out.println("Area of triangle y: " + y.calcArea());
 
-        if(x.area > y.area){
+        if(x.calcArea() > y.calcArea()){
             System.out.println("large: X");
         } else {
             System.out.println("large: Y");
