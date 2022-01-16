6d9473190015c12082dbfbdc579dc4aec73c8a1d
Fredson Chaves <fredsonchaves07@gmail.com>
2022-01-16 14:24:17 -0300

    feat: add forEach example

---
+++ src/arrays/forEach/Program.java
@@ -0,0 +1,13 @@
+package arrays.forEach;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        String[] nameVect = {"Maria", "Bob", "Alex"};
+
+        for(String vect: nameVect){
+            System.out.println(vect);
+        }
+    }
+}
