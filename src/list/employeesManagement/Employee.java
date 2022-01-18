87f4ffeb6e83fc1f54f1c20d2204ac66679c89de
Fredson Chaves <fredsonchaves07@gmail.com>
2022-01-18 12:36:26 -0300

    feat: add employe management

---
+++ src/list/employeesManagement/Employee.java
@@ -0,0 +1,31 @@
+package list.employeesManagement;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void increaseSalary(double percentage) {
+        this.salary += (this.salary * percentage / 100);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
