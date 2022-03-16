beca6134a010e78370ef263cda7dbd6197973cfb
Fredson Ribeiro <fredsonchaves07@gmail.com>
2022-03-16 13:55:13 -0300

    feat: solution with interface

--- src/interfaces/model/services/RentalService.java
+++ src/interfaces/model/services/RentalService.java
@@ -7,11 +7,11 @@ public class RentalService {
 
     private Double pricePerDay;
     private Double pricePerHour;
-    private BazilTaxService taxService;
+    private TaxService taxService;
 
     public RentalService() {}
 
-    public RentalService(Double pricePerDay, Double pricePerHour, BazilTaxService taxService) {
+    public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
         this.pricePerDay = pricePerDay;
         this.pricePerHour = pricePerHour;
         this.taxService = taxService;
