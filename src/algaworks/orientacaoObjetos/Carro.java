f4f2c0fb0f66a1ad49e8e1ca8b37cee04b43c286
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-07-27 12:19:43 -0300

    feat: adicionando metodo

--- src/algaworks/orientacaoObjetos/Carro.java
+++ src/algaworks/orientacaoObjetos/Carro.java
@@ -1,10 +1,22 @@
 package algaworks.orientacaoObjetos;
 
+import java.time.LocalDateTime;
+
 public class Carro {
 
+    public static final int VIDA_UTIL_ANOS = 20;
     String fabricante;
     String modelo;
     String cor;
     int anoFabricacao;
+    double precoCompra;
     Pessoa proprietario;
+
+    void calcularValorRevenda() {
+        int tempoDeUsoEmAnos = LocalDateTime.now().getYear() - anoFabricacao;
+        double valorRevenda = (precoCompra / VIDA_UTIL_ANOS) * (20 - tempoDeUsoEmAnos);
+        if (valorRevenda < 0) valorRevenda = 0;
+        System.out.println("Tempo de uso (anos): " + tempoDeUsoEmAnos);
+        System.out.println("Valor de revenda: " + valorRevenda);
+    }
 }
